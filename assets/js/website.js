2ae249d4345146fe5bad1ee4453baf8d36e5ac29
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-08-08 12:42:38 -0700

    A few changes here and there

---
+++ assets/js/website.js
@@ -0,0 +1,5 @@
+$("#progressBarAnimation").click(function () {
+  // $(".progress-bar").addClass("progress-bar-animation");
+  let width = prompt("What percentage do you change he progress bar to");
+  $(".progress-bar").attr("style", `width: ${width}`);
+});
--- assets/js/website/Main.ts
+++ assets/js/website/Main.ts
@@ -11,8 +11,3 @@ let population: string[] = ["Bob", "John"];
 $("#startGame").click(function () {
   let game = new Game(5000, 1, population);
 });
-
-$("#progressBarAnimation").click(function () {
-  // $(".progress-bar").addClass("progress-bar-animation");
-  $(".progress-bar").attr("style", "width: 100%");
-});
