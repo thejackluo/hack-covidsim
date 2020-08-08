ba07f3858e0ed3bdfcc6816662805e8b21d466ac
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-08-08 11:05:08 -0700

    Some changes

--- assets/js/website/Game.ts
+++ assets/js/website/Game.ts
@@ -1,47 +1,41 @@
-interface GameVariables {
+export class CovidGame {
+  constructor(
+    private totalPeople: number,
+    private numPeopleRecovered: number,
+    private numPeopleDead: number,
+    private numPeopleInfected: number,
+    private time: number,
+    private populationArray: number[]
+  ) {}
 
-}
+  // public constructor(numPeopleInfected, totalPeople, R) {
+  //   this.numPeopleInfected = numPeopleInfected;
+  //   this.totalPeople = totalPeople;
+  //   this.R = R;
+  //   for (let i = 0; i < totalPeople; i++) {}
+  // }
 
-public class CovidGame {
-  let time = 0
-  let numPeopleRecovered = 0;
-  let numPeopleDead = 0;
-
-  let R;
-  // integer
-  const totalPeople;
-  let numPeopleInfected;
-  // array
-  let population;
-
-  public constructor(numPeopleInfected, totalPeople, R) {
-    this.numPeopleInfected = numPeopleInfected;
-    this.totalPeople = totalPeople;
-    this.R = R;
-    for (let i = 0; i < totalPeople; i++) {}
-  }
-
-  public incrementTime(time) {
-    for (let j = 0; j < population.length; j++) {
-      let person = population[j].getPerson();
-      for (let k = 0; k < person.contactsPerDay; k++) {
-        let contactedPerson =
-          population[Math.floor(Math.random() * population.length)];
-        if (person.isInfected() && !person.infectedToday()) {
-          let randomNumber = Math.floor(Math.random() * 100);
-          if (randomNumber < person.chanceToSpread() * 100) {
-            contactedPerson.infect();
-          }
-        } else if (
-          contactedPerson.isInfected() &&
-          !contactedPerson.infectedToday()
-        ) {
-          let randomNumber = Math.floor(Math.random() * 100);
-          if (randomNumber < contactedPerson.chanceToSpread() * 100) {
-            person.infect();
-          }
-        }
-      }
-    }
+  public incrementTime(time): void {
+    // for (let j = 0; j < this.populationArray.length; j++) {
+    //   let person = populationArray[j].getPerson();
+    //   for (let k = 0; k < person.contactsPerDay; k++) {
+    //     let contactedPerson =
+    //       populationArray[Math.floor(Math.random() * populationArray.length)];
+    //     if (person.isInfected() && !person.infectedToday()) {
+    //       let randomNumber = Math.floor(Math.random() * 100);
+    //       if (randomNumber < person.chanceToSpread() * 100) {
+    //         contactedPerson.infect();
+    //       }
+    //     } else if (
+    //       contactedPerson.isInfected() &&
+    //       !contactedPerson.infectedToday()
+    //     ) {
+    //       let randomNumber = Math.floor(Math.random() * 100);
+    //       if (randomNumber < contactedPerson.chanceToSpread() * 100) {
+    //         person.infect();
+    //       }
+    //     }
+    //   }
+    // }
   }
 }
--- assets/js/website/Main.ts
+++ assets/js/website/Main.ts
@@ -2,9 +2,9 @@
 	Jack Luo Website by Jack
 	www.jack-luo.com | @thejackluo
 */
-import { Game } from "./Game";
+import { CovidGame } from "./CovidGame";
 import $ from "jquery";
 
 $("#startGame").click(function startGame() {
-  let newGame = (Game) => {};
+  let game = new CovidGame();
 });
