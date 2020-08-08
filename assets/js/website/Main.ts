21435d47aa88b2735edc277bd9c28ee569a4f61f
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-08-08 12:17:44 -0700

    Progress bar changes

--- assets/js/website/Main.ts
+++ assets/js/website/Main.ts
@@ -8,10 +8,10 @@ import $ from "jquery";
 // We need a method to generate names
 let population: string[] = ["Bob", "John"];
 
-$("#startGame").click(function startGame() {
+$("#startGame").click(function () {
   let game = new Game(5000, 1, population);
 });
 
-$("#startGame").click(function startGame() {
-  let game = new Game(5000, 1, population);
+$("#progresBarAnimation").click(function () {
+  $("#progresBarAnimation").toggleClass("");
 });
