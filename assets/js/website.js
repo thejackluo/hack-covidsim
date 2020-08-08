de8a9a88747a8c2cc691b57a85923706c7670794
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-08-08 13:00:26 -0700

    Node Module Changes

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,5 +1,6 @@
 $("#progressBarAnimation").click(function () {
   // $(".progress-bar").addClass("progress-bar-animation");
   let width = prompt("What percentage do you change he progress bar to");
-  $(".progress-bar").attr("style", `width: ${width}`);
+  $(".progress-bar").attr("style", `width: ${width}%`);
 });
+a;
