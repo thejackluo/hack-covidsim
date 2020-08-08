57769625c984b4f345e06dfe11d298663b4511c9
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-08-08 11:58:54 -0700

    Main.css change

--- assets/js/website/Person.ts
+++ assets/js/website/Person.ts
@@ -1,7 +1,43 @@
 export class Person {
   public constructor(
     private isEducated: boolean,
+    private isInfected: boolean,
     private isWealthy: boolean,
     private daysLeft: number
   ) {}
+
+  public getInfected(): boolean {
+    return this.isInfected;
+    // returns if is infected
+  }
+
+  public getEducated(): boolean {
+    return;
+
+    // returns if is educated
+  }
+
+  public incrementDay(): void {
+    // decrease day, infectedToday = false
+  }
+
+  public remainingDays(): number {
+    return;
+
+    // returns remaining days
+  }
+
+  public survives(): boolean {
+    return;
+
+    // return true if RNG give number < chanceOf survival
+  }
+
+  public infection(): void {
+    // sets days to a range between 6 and 21, setsisInfected to true, infectedToday to true
+  }
+
+  public update(): void {
+    return;
+  }
 }
